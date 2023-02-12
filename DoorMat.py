24c240705d475f82971aada60fc198ff04ae7b34
TejasJogi <tejasjogi1995@gmail.com>
2023-02-12 15:53:07 +0530

    Create a door mat pattern

--- DoorMat.py
+++ DoorMat.py
@@ -5,4 +5,9 @@ f = '.|.'
 welcome = 'WELCOME'
 
 for i in range(1,N,2):       
-    print(f.center(M, h))
+    print((f*i).center(M, h))
+
+print(welcome.center(M,h))
+
+for i in range(1,N,2):       
+    print((f*(N-i-1)).center(M, h))
