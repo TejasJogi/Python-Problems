db7cc53476bb493b03c4205f03711d372c0bc472
TejasJogi <tejasjogi1995@gmail.com>
2023-02-15 20:10:33 +0530

    Make a design with Alphabets

---
+++ AlphabetRangoli.py
@@ -0,0 +1,13 @@
+def print_rangoli(size):
+    # your code goes here
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    string = []
+    for i in range(size):
+        e = "-".join(alphabet[i:size])
+        string.append((e[::-1]+e[1:]).center(4*size-3, "-"))
+    print('\n'.join(string[:0:-1]+string))
+
+
+if __name__ == '__main__':
+    n = int(input())  
+    print_rangoli(n)
