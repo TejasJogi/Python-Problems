085bf876d1ced99d432d174acfc2333f95191765
TejasJogi <tejasjogi1995@gmail.com>
2023-02-22 20:49:00 +0530

    Validate phone number by regular expression

--- Regex-ValidatePhoneNumber.py
+++ Regex-ValidatePhoneNumber.py
@@ -4,7 +4,7 @@ N = int(input())
 
 for i in range(N):
     num = input()
-    res = re.search(r"\b[7-9]{1}[0-9]{2,9}\b", num)
+    res = re.search(r"\b[7-9]{1}[0-9]{9}\b", num)
     if res:
         print('YES')
     else:
