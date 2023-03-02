fb6f2601a9da96face23c985f0753b54a130721d
TejasJogi <tejasjogi1995@gmail.com>
2023-03-02 21:10:17 +0530

    Linearalgebra problemusing numpy

---
+++ Numpy-LinearAlgebra.py
@@ -0,0 +1,33 @@
+# Task
+
+# You are given a square matrix A with dimensions NXN. Your task is to find the determinant. Note: Round the answer to 2 places after the decimal.
+
+# Input Format
+
+# The first line contains the integer N.
+# The next N lines contains the N space separated elements of array A.
+
+# Output Format
+
+# Print the determinant of A.
+
+# Sample Input
+
+# 2
+# 1.1 1.1
+# 1.1 1.1
+
+# Sample Output
+
+# 0.0
+
+import numpy
+
+
+
+N = int(input())
+
+arr = numpy.array([list(map(float, input().split())) for i in range(N)])
+
+numpy.set_printoptions(legacy='1.13')
+print(numpy.linalg.det(arr))
