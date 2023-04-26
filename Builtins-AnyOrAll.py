3df9ba09b86e35cd6d7e3b67f42c2b28d23ff63e
TejasJogi <tejasjogi1995@gmail.com>
2023-04-26 20:44:09 +0530

    find positive or  palindrom integers from given integers

---
+++ Builtins-AnyOrAll.py
@@ -0,0 +1,28 @@
+# Task
+
+# You are given a space separated list of integers. If all the integers are positive, then you need to check if any integer is a palindromic integer.
+
+# Input Format
+
+# The first line contains an integer N. N is the total number of integers in the list.
+# The second line contains the space separated list of N integers.
+
+# Output Format
+
+# Print True if all the conditions of the problem statement are satisfied. Otherwise, print False.
+
+# Sample Input
+
+# 5
+# 12 9 61 5 14 
+
+# Sample Output
+
+# True
+
+
+N = int(input())
+
+integers = list(map(int, input().split()))
+
+print(True if any(integers) and all(integers) == True else False)
