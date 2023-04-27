2421b9291a282c175ba7b580159f9b043214b02b
TejasJogi <tejasjogi1995@gmail.com>
2023-04-27 20:03:35 +0530

    Check positive number and palindrom numbers in given integers

--- Builtins-AnyOrAll.py
+++ Builtins-AnyOrAll.py
@@ -23,6 +23,6 @@
 
 N = int(input())
 
-integers = list(map(int, input().split()))
+integers = input().split()
 
-print(True if any(integers) and all(integers) == True else False)
+print(all(int(i) >= 0 for i in integers) and any(i == i[::-1] for i in integers))
